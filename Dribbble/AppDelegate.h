f5a3aabd9e043c73c4de8cdd860d6c595a75157e
john andrews <johnnydrews@gmail.com>
2014-01-23 09:27:56 -0500

    Initial Commit

---
+++ Dribbble/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Dribbble
+//
+//  Created by John Andrews on 1/23/14.
+//  Copyright (c) 2014 John Andrews. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Dribbble/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Dribbble
+//
+//  Created by John Andrews on 1/23/14.
+//  Copyright (c) 2014 John Andrews. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
